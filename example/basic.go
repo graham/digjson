adcc8420c088d147f01a39424a01e21e59ca58c7
Graham Abbott <graham.abbott@gmail.com>
2021-12-24 21:41:34 -0800

    add an example

---
+++ example/basic.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/graham/digjson"
+)
+
+func main() {
+	fmt.Println("hello world")
+
+	//jsondata_with_nil := []byte(`{ "user": null }`)
+	jsondata_with_obj := []byte(`{ "user": { "username": "user1" } }`)
+	jsondata_with_array := []byte(`{"user": [{ "username": "user1"}, {"username": "user2"} ] }`)
+
+	type User struct {
+		Username string `json:"username"`
+	}
+
+	var users []User
+	was_found, err := digjson.Dig(jsondata_with_obj, "user", &users)
+
+	fmt.Println(users, was_found, err)
+
+	users = []User{}
+	was_found, err = digjson.Dig(jsondata_with_array, "user", &users)
+	fmt.Println(users, was_found, err)
+
+}
