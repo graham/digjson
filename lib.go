87ca932312e4804f3db28cf808a7665c3b6d6138
Graham Abbott <graham.abbott@gmail.com>
2021-12-24 20:43:55 -0800

    comments round 1.

--- lib.go
+++ lib.go
@@ -9,6 +9,10 @@ import (
 	"strings"
 )
 
+// Used to dig into the json document and get to the value you're looking for.
+// The assumption is that inner_dig is getting the standard map[string]interface{}
+// that json.Unmarshal will return if asked (likely works with other json decoders)
+// but I haven't tried it.
 func inner_dig(obj map[string]interface{}, rest string) (interface{}, bool) {
 	chunks := strings.SplitN(rest, ".", 2)
 
@@ -30,6 +34,9 @@ func inner_dig(obj map[string]interface{}, rest string) (interface{}, bool) {
 
 }
 
+// Since we've already decoded the json, we're long past the json module assigning
+// fields with the `json` tags. This method transfers those directives to a struct.
+// For now it doesn't support some of the more complicated directives, just the json name.
 func TransferMapToStruct(data map[string]interface{}, target interface{}) error {
 	tType := reflect.TypeOf(target)
 	tVal := reflect.ValueOf(target)
@@ -60,11 +67,22 @@ func TransferMapToStruct(data map[string]interface{}, target interface{}) error 
 	return nil
 }
 
+// Similar to the above, if the user provided "target" for the data is a slice
+// we have to generate the correct data and write it into the slice.
 func TransferResultToSlice(data interface{}, target interface{}) error {
+	// It is possible that the data path we used to dig into the
+	// json document lead us to a null value, in that case.
+	// In this case, don't touch the variable and return no error.
+	// It might make more sense to clear the array in this case,
+	// as someone could be re-using the slice, but for now we'll
+	// just leave things alone.
 	if data == nil {
 		return nil
 	}
 
+	// Our dig path may have lead us to a {} (object), in that case,
+	// We still want to write into the slice, but we want to write
+	// one value into the list.
 	unboxed, ok := data.(map[string]interface{})
 
 	if ok {
@@ -79,6 +97,10 @@ func TransferResultToSlice(data interface{}, target interface{}) error {
 		return nil
 	}
 
+	// This is what nice APIs do, they will return a list no matter
+	// what. In this case, we simply want to make sure we are creating
+	// the right struct and copying the information into the correct
+	// fields.
 	unsliced, ok := data.([]interface{})
 
 	if ok {
@@ -99,14 +121,25 @@ func TransferResultToSlice(data interface{}, target interface{}) error {
 
 }
 
+// Allow implicit conversions if the target struct has a field of a type that
+// can be easily converted, for example, string to int is implemented,
+// it would be trivial to implement more conversions.
 func Dig(payload []byte, path string, target interface{}) (bool, error) {
 	return dig(payload, path, target, false)
 }
 
+// Don't allow any conversions, if the source (json) type doesn't match the
+// target (struct) type, bail out.
 func DigStrict(payload []byte, path string, target interface{}) (bool, error) {
 	return dig(payload, path, target, true)
 }
 
+// The most important part of the implementation, determine the source and target types
+// determine if any conversion should happen, and make sure that it happens without error.
+// Because in this case, your return value could be null as the "correct" value, it's
+// important to know if the dig path actually resolved in the json document, or if some
+// other issue occured. This is the bool returned from this method.
+// (was_the_path_found, and error).
 func dig(payload []byte, path string, target interface{}, isStrict bool) (bool, error) {
 	var t map[string]interface{}
 	err := json.Unmarshal(payload, &t)
